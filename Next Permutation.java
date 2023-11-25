ec7fb881f3b0d825a675704b86fc1e8f480f1700
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-11-25 22:52:21 +0530

    leet

--- Next Permutation.java
+++ Next Permutation.java
@@ -1,37 +1,33 @@
-import java.util.* ;
-import java.io.*; 
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class Solution 
-{
-	public static ArrayList<Integer> nextPermutation(ArrayList<Integer> ar) 
-	{
-		// Write your code here.
-		int hello = -1;
-		for(int i=ar.size()-2;i>=0;i--){
-			if(ar.get(i+1)>ar.get(i)){
-				hello=i;
-				break;
-			}
-		}
-		if(hello==-1){
-			ar.sort(Comparator.naturalOrder());
-			return ar;
-		}
-		for(int i=ar.size()-1;i>=0;i--){
-			if(ar.get(hello)<ar.get(i)){
-				Collections.swap(ar, hello, i);
-				break;
-			}
-		}
-		int i=hello+1;
-		int j = ar.size()-1;
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int hello = -1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                hello=i;
+                break;
+            }
+        }
+        if(hello==-1){
+            Arrays.sort(nums);
+            return;
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]>nums[hello]){
+                int temp = nums[hello];
+                nums[hello]=nums[i];
+                nums[i]=temp;
+                break;
+            }
+        }
+        int i=hello+1,j=nums.length-1;
         while(i<j){
-			Collections.swap(ar, i, j);
-			i++;
-			j--;
-		}
-		return ar;
-	}
+            int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
+        }
+        return;
+        
+    }
 }
